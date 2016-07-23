01c59259bb67cd24566795d714def0f6eefa4aa9
Lee ZG <Lzg__@github.com>
2016-07-23 14:49:58 +0800

    first commit to text

---
+++ weekend/weekend/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  weekend
+//
+//  Created by 李振刚 on 16/7/23.
+//  Copyright © 2016年 李振刚. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ weekend/weekend/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  weekend
+//
+//  Created by 李振刚 on 16/7/23.
+//  Copyright © 2016年 李振刚. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
