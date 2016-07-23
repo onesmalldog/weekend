57427ee84dea6fe4a3a71e9fa54924894db04790
Lee ZG <Lzg__@github.com>
2016-07-23 16:11:37 +0800

    source 'https://github.com/CocoaPods/Specs.git'
    platform :ios, '8.0'
    use_frameworks!
    
    target ‘DTEN’ do
        pod ‘MBProgressHUD’
        pod ‘AFNetworking’
        pod ’SDWebImage’
    end

---
+++ weekend/Pods/Target Support Files/Pods-weekend/Pods-weekend-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_weekend : NSObject
+@end
+@implementation PodsDummy_Pods_weekend
+@end
--- weekend/weekend/LGMainController.h
+++ weekend/weekend/LGMainController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  LGMainController.h
 //  weekend
 //
 //  Created by 李振刚 on 16/7/23.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface LGMainController : UITabBarController
 
 @end
-
